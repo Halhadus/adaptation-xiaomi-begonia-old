dd1e3233079af10d6e79902cb80b004aab282ebb
Halhadus <eneshakans45@proton.me>
2024-08-09 12:52:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 cp -rf /adaptation-xiaomi-begonia/* /
 cp -rf /adaptation-xiaomi-begonia/etc/environment.d/environment /etc/environment
 rm /etc/ofono/ril_subscription.conf
+chmod 755 /usr/bin/pbhelper
 systemctl disable droidian-fpd
 systemctl enable batman modules ssh-fix pbhelper droidian-perf brightness
 bash /adaptation-xiaomi-begonia/groups.sh
