68d2067530e4413a8e5fb715ce274e2a03cefe6f
Halhadus <eneshakans45@proton.me>
2024-09-06 00:50:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Run as root!
+mkdir -p /tmp/run/systemd/resolve
+echo "nameserver 9.9.9.11" > /tmp/run/systemd/resolve/stub-resolv.conf
+mount --bind /tmp/run /run
+apt update
+apt python3-evdev -y
+umount /run
+rm -rf /tmp/run
+
 cp -rf /adaptation-xiaomi-begonia/* /
 cp -rf /adaptation-xiaomi-begonia/etc/environment.d/environment /etc/environment
 rm /etc/ofono/ril_subscription.conf
